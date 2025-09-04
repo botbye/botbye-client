c747e87279ca561ea3246c073df1178d9d7ad69d
botbye <accounts@botbye.com>
2025-09-04 14:57:50 +0300

    update

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,9 @@ type TInitChallengesOptions = {
     api?: string;
     clientKey: string;
     withoutSessions?: boolean;
+    withoutReload?: boolean;
+    withoutRemoteStorage?: boolean;
 };
 declare const setUserId: (userId: string) => void, initChallenges: ({ api, clientKey, withoutSessions, }: TInitChallengesOptions) => Promise<TChallengesRunner>, runChallenge: TChallengesRunner;
 export { initChallenges, runChallenge, setUserId, };
+export type {TInitChallengesOptions, TGetTokenOptions}
