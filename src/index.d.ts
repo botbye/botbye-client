127413c418d0b5e4ba450d12d8b90bd9f5353469
botbye <accounts@botbye.com>
2026-06-23 16:56:15 +0300

    AP api

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,22 @@ type TGetTokenOptions = {
     clickTag?: string;
 };
 type TChallengesRunner = (options?: TGetTokenOptions) => PromiseLike<string>;
+type TPhishingInitOptions = {
+    api?: string;
+    clientKey: string;
+};
+type TPhishingCatcherGetterOptions = {
+    url?: string;
+    id?: string;
+    type?: "PNG" | "OBJECT";
+    skipExecution?: boolean;
+    innerUrl?: string;
+};
+type TPhishingCatcherGetter = (options?: TPhishingCatcherGetterOptions) => HTMLElement;
+type TPhishingApi = {
+    getCatcher: TPhishingCatcherGetter;
+};
+type TPhishingInit = (options: TPhishingInitOptions) => PromiseLike<TPhishingApi>;
 type TInitChallengesOptions = {
     api?: string;
     clientKey: string;
@@ -14,7 +30,8 @@ type TFactoryResponse = {
     initChallenges: (options: TInitChallengesOptions) => PromiseLike<TChallengesRunner>;
     runChallenge: TChallengesRunner;
     setUserId: TSetUserId;
+    initPhishing: TPhishingInit;
 };
-declare const factory: (url?: string) => TFactoryResponse;
-declare const initChallenges: (options: TInitChallengesOptions) => PromiseLike<TChallengesRunner>, runChallenge: TChallengesRunner, setUserId: TSetUserId;
-export { initChallenges, runChallenge, setUserId, factory, type TGetTokenOptions, type TChallengesRunner, type TInitChallengesOptions, type TSetUserId, type TFactoryResponse, };
+declare const factory: (api?: string) => TFactoryResponse;
+declare const initChallenges: (options: TInitChallengesOptions) => PromiseLike<TChallengesRunner>, runChallenge: TChallengesRunner, setUserId: TSetUserId, initPhishing: TPhishingInit;
+export { initChallenges, runChallenge, setUserId, factory, initPhishing, type TGetTokenOptions, type TChallengesRunner, type TInitChallengesOptions, type TSetUserId, type TFactoryResponse, };
